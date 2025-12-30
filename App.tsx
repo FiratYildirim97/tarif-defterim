7be345784242922d84af55339a19a38f105d5cb3
FiratYildirim97 <tx72hfm9s9@privaterelay.appleid.com>
2025-12-30 23:36:36 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -281,6 +281,7 @@ const App: React.FC = () => {
             element={
               <FirebaseConfigScreen
                 setFirebaseConfig={updateFirebaseConfig}
+                onAddConfig={addConfig}
               />
             }
           />
